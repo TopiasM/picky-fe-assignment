11f7b6acd49339c51f37ed0b3bf862509927fe37
TopiasM <topias.martikainen@gmail.com>
2024-07-13 22:49:41 +0900

    Project setup

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center gap-2">
+      <a href="/discussions">
+        <h1 className="text-2xl dark:text-white border-b-2 border-pink-400 border-solid hover:border-b-4">Discussions page</h1>
+      </a>
+    </main>
+  );
+}
